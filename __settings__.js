2dbf9f9191b4fd25643e372fb7e9d4cf101b9b54
FOUREX <Foxtrotserega@gmail.com>
2024-03-08 19:11:42 +0100

    Yep

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1965002.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 170647185, 170647206, 170647188, 170647189 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/170647190/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/170647215/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/170647195/1/ammo.js', 'preload' : true},
+];
